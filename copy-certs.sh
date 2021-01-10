c9192d4e622afc2bb709e10c12a748ef8f2b3142
Michael Treadwell <mich@eltreadwell.com>
2021-01-09 19:24:57 -0600

    fix automation scripts

--- copy-certs.sh
+++ copy-certs.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo "Stopping apache webserver..."
 sudo service apache2 stop
-sudo certbot renew -n -q
+echo "Apache webserver stopped..."
 
+echo "Renewing certificates..."
+sudo certbot renew -n -q
+echo "Certificates renewed..."
+
+echo "Configuring certificates..."
 sudo cp /etc/letsencrypt/live/irc.assimilate.dev/fullchain.pem /home/ubuntu/inspircd-2.0.29/run/conf/cert.pem
 sudo cp /etc/letsencrypt/live/irc.assimilate.dev/privkey.pem /home/ubuntu/inspircd-2.0.29/run/conf/key.pem
 sudo cp /etc/letsencrypt/live/irc.assimilate.dev/chain.pem /home/ubuntu/inspircd-2.0.29/run/conf/ca.pem
@@ -10,7 +16,9 @@ sudo cp /etc/letsencrypt/live/irc.assimilate.dev/chain.pem /home/ubuntu/inspircd
 sudo cp /etc/letsencrypt/live/irc.assimilate.dev/fullchain.pem /etc/ssl/certs/irc.assimilate.dev.crt
 sudo cp /etc/letsencrypt/live/irc.assimilate.dev/privkey.pem /etc/ssl/private/irc.assimilate.dev.key
 sudo cp /etc/letsencrypt/live/irc.assimilate.dev/chain.pem /etc/ssl/certs/ca-certificates.crt
+echo "Certificates configured..."
 
+echo "Restarting services..."
 sudo service apache2 start
-/home/ubuntu/inspircd-2.0.29/run/inspircd start
-/home/ubuntu/inspircd-2.0.29/run/services/bin/anoperc start
+/home/ubuntu/inspircd-2.0.29/run/inspircd rehash
+echo "Renewal complete..."
